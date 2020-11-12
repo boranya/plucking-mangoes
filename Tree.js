fe6052acbecc4df07dbbe3a3e692d6dc0f51ce99
boranya <72442807+boranya@users.noreply.github.com>
2020-11-12 23:58:41 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,12 @@
+class{
+    constructor(x, y){
+        var options={
+            isStatic: true,
+
+        }
+        this.treeImage=loadImage("tsprites/tree.png");
+    }
+    display(){
+
+    };
+}
